4f32033c47a36dc4f72c552a3b05f44be8ac9a6e
lakshyamodi832 <lakshyaymodi@gmail.com>
2025-12-02 02:55:56 +0530

    dynamicPolymorphism

--- src/com/demo/oops/polyomrphism.java
+++ src/com/demo/oops/polyomrphism.java
@@ -9,9 +9,35 @@ public class polyomrphism {
         return a+b+c;
     }
 
-    public static void main(String[] args){
-        polyomrphism calc = new polyomrphism();
-        System.out.println(calc.add(2,3));
-        System.out.println(calc.add(2, 3, 4));
+//    public static void main(String[] args){
+//        polyomrphism calc = new polyomrphism();
+//        System.out.println(calc.add(2,3));
+//        System.out.println(calc.add(2, 3, 4));
+//    }
+
+    class Animal{
+        void sound(){
+            System.out.println("*animal noises");
+        }
+    }
+
+    class Dog extends Animal{
+        void sound(){
+            System.out.println("Barks");
+        }
+    }
+
+    class Cow extends Animal{
+        void sound(){
+            System.out.println("Moo");
+        }
+    }
+
+    public void main(String[] args){
+        Animal animal1 = new Dog();
+        Animal animal2 = new Cow();
+
+        animal1.sound();
+        animal2.sound();
     }
 }
