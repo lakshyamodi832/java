74532f4f872d2101682974968f54c75fb10348fa
lakshyamodi832 <lakshyaymodi@gmail.com>
2025-12-01 23:49:54 +0530

    .

--- src/com/demo/oops/Car.java
+++ src/com/demo/oops/Car.java
@@ -1,5 +1,11 @@
 package com.demo.oops;
 
 public class Car {
+    int speed;
+    String color;
+
+    void drive(){
+        System.out.println("Driving");
+    }
 
 }
--- src/com/demo/oops/test.java
+++ src/com/demo/oops/test.java
@@ -1,4 +1,10 @@
 package com.demo.oops;
 
 public class test {
+    public static void main(String[] args){
+        Car car = new Car();
+        car.speed = 69;
+        car.color = "White";
+        car.drive();
+    }
 }
