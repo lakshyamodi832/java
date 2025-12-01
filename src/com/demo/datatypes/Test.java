84cbf0edb4b767b289486f9e1657f6874705f057
lakshyamodi832 <lakshyaymodi@gmail.com>
2025-12-01 18:31:04 +0530

    conditional

--- src/com/demo/datatypes/Test.java
+++ src/com/demo/datatypes/Test.java
@@ -29,7 +29,44 @@ public class Test {
          boolean isRaining = true;
          boolean isBadWeather = isSnowing || isRaining;
          System.out.println("Bad Weather = " + isBadWeather);//true
-         boolean israining = false;
-         System.out.println("Beach Day = " + !israining);//true
+         isRaining = false;
+         System.out.println("Beach Day = " + !isRaining);//true
+
+         isWarm = false;;
+         if(isSnowing && isWarm){
+             System.out.println("Beach Day");
+         } else {
+             System.out.println("Bad Weather");
+         }
+
+         if(isRaining || isSnowing){
+             System.out.println("Bad Weather");
+         }
+
+         int day = 2;
+         String dayName;
+
+         if(day == 1){
+             System.out.println("Monday");
+         } else if(day == 2){
+             System.out.println("Tuesday");
+         } else if(day == 3){
+             System.out.println("Wednesday");
+         }//Tuesday
+
+         switch(day){
+             case 1:
+                 dayName = "Monday";
+                 break;
+             case 2:
+                 dayName = "Tuesday";
+                 break;
+             case 3:
+                 dayName = "Wednesday";
+
+             System.out.println(dayName);//Tuesday
+         }
+
+         dayName = (day == 2) ? "Tuesday":"Monday";
      }
 }
