d2cc6781b8a8671b0c2c7b18e4cf892e9cfdb086
lakshyamodi832 <lakshyaymodi@gmail.com>
2025-12-02 00:59:02 +0530

    inheritance inheritance

--- src/com/demo/oops/test.java
+++ src/com/demo/oops/test.java
@@ -1,8 +1,28 @@
 package com.demo.oops;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class test {
+    static class Animal{
+        String name;
+        void eat(){
+            System.out.println(name + " is eating.");
+        }
+    }
+
+    static class Dog extends Animal{
+        void bark(){
+            System.out.println(name + "is barking.");
+        }
+    }
     public static void main(String[] args){
 //        Car car = new Car("White");
 //        car.setSpeed(69);
+
+        Dog dog = new Dog();
+        dog.name = "Tommy";
+        dog.eat();
+        dog.bark();
+
     }
 }
