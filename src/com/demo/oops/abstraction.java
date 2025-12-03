c12919da1046ba3ea42cc1f5df2a13f72cdf7776
lakshyamodi832 <lakshyaymodi@gmail.com>
2025-12-03 18:34:20 +0530

    abstraction

---
+++ src/com/demo/oops/abstraction.java
@@ -0,0 +1,32 @@
+package com.demo.oops;
+
+public class abstraction {
+    public void main(String[] args){
+        Dog dog1 = new Dog();
+        dog1.Hello();
+        dog1.Bye();
+        dog1.sleep();
+    }
+}
+
+abstract class Animal{
+    abstract void Hello();
+    abstract void Bye();
+
+    void sleep(){
+        System.out.println("zzz...");
+    }
+}
+
+class Dog extends Animal{
+
+    @Override
+    void Hello() {
+        System.out.println("hi");
+    }
+
+    @Override
+    void Bye() {
+        System.out.println("bye");
+    }
+}
