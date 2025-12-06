7849670673ebb8e6a1c439d559bc54a35772fad2
lakshyamodi832 <lakshyaymodi@gmail.com>
2025-12-06 06:24:19 +0530

    multithreading

---
+++ src/com/demo/multithreading/m_t.java
@@ -0,0 +1,10 @@
+package com.demo.multithreading;
+
+import static java.lang.Character.getName;
+
+public class m_t {
+    public static void main(String[] args){
+        System.out.println("Hello!");
+        System.out.println(Thread.currentThread().getName());
+    }
+}
